cdd8a996044fa4f067e7d7403616f0fd46a045e5
Shai Nagar <shain@wix.com>
2018-08-22 16:43:42 +0300

    setting appVersion at packaging time with --app-version

--- helm-package.sh
+++ helm-package.sh
@@ -1,9 +1,20 @@
 #!/usr/bin/env bash
 
+source "./include.sh"
+
+if [ "$1" = "" ]
+then
+    bold "Enter an appVersion: (we will use one app ver for all releases just for the demo)"
+    read APPVER
+else
+    APPVER=$1
+fi
+
+
 PKG_DIR=packages
 
 mkdir $PKG_DIR
-helm package charts/global -d $PKG_DIR
-helm package charts/namespace -d $PKG_DIR
-helm package charts/echo-server -d $PKG_DIR
-helm package charts/nesting-example -d $PKG_DIR
+helm package charts/global --app-version $APPVER -d $PKG_DIR
+helm package charts/namespace --app-version $APPVER -d $PKG_DIR
+helm package charts/echo-server --app-version $APPVER -d $PKG_DIR
+helm package charts/nesting-example --app-version $APPVER -d $PKG_DIR
